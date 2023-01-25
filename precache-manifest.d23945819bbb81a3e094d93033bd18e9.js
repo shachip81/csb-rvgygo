a37f236e1ef3b4ee7fa99cd233584f49ec9ab0a6
Code Sandbox <hello@codesandbox.io>
2023-01-25 04:02:15 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d23945819bbb81a3e094d93033bd18e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1126493b3cda20625b71",
+    "url": "https://shachip81.github.io/csb-rvgygo/static/css/main.7ee8d537.chunk.css"
+  },
+  {
+    "revision": "1126493b3cda20625b71",
+    "url": "https://shachip81.github.io/csb-rvgygo/static/js/main.1126493b.chunk.js"
+  },
+  {
+    "revision": "0b8fc25b66a396db6104",
+    "url": "https://shachip81.github.io/csb-rvgygo/static/css/1.97bc9538.chunk.css"
+  },
+  {
+    "revision": "0b8fc25b66a396db6104",
+    "url": "https://shachip81.github.io/csb-rvgygo/static/js/1.0b8fc25b.chunk.js"
+  },
+  {
+    "revision": "48493696bd7033954623",
+    "url": "https://shachip81.github.io/csb-rvgygo/static/js/runtime~main.48493696.js"
+  },
+  {
+    "revision": "caf62af9f1250796670c0bd67ff07224",
+    "url": "https://shachip81.github.io/csb-rvgygo/index.html"
+  }
+];
